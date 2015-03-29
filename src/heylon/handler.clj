7f062d1b419fa3697cb762b6b5d17391ee2d515d
juszczakn <juszczakn@gmail.com>
2015-03-29 12:41:02 -0500

    login form

--- src/heylon/handler.clj
+++ src/heylon/handler.clj
@@ -1,11 +1,19 @@
 (ns heylon.handler
+  (:use [clojure.java.io :only [resource]])
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [heylon.db.login :as login]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (route/files "heylon/login.html")
+  (route/files "heylon/index.html")
+  (POST "/heylon/login" {params :params} (str (login/check-login (:email params) (:password params))))
   (route/not-found "Not Found"))
 
+(def my-site-defaults
+  (assoc site-defaults :security
+         (assoc (site-defaults :security) :anti-forgery false)))
+
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes my-site-defaults))
