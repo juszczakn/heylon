f8f0ddb1aa0123282f411519b09827fdab9d945f
juszczakn <juszczakn@gmail.com>
2015-08-16 15:39:41 -0500

    update dependencies to latest versions

--- project.clj
+++ project.clj
@@ -2,19 +2,19 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [ring/ring-json "0.3.1"]
-                 [org.clojure/java.jdbc "0.3.6"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/java.jdbc "0.4.1"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
                  [crypto-password "0.1.3"]
                  ;;[org.clojure/clojurescript "0.0-2127"]
                  ]
-  :plugins [[lein-ring "0.8.13"]
+  :plugins [[lein-ring "0.9.6"]
             ;;[lein-cljsbuild "1.0.1"]
             ]
   :ring {:handler heylon.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring/ring-mock "0.2.0"]]}})
